ebcc8659814e1c613d73a5a217a6d03f105b1478
zeleffoff <raphaelamoro330@gmail.com>
2023-02-20 20:24:54 +0100

    Errors return changed

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -35,27 +35,28 @@ class Scheduler extends EventEmitter {
         this.autoPost = options.autoPost;
         this.database = null;
 
-        if (!this.mongoUri) _error("Uri MongoDB manquant.", 'INVALID_MONGO');
-        if (!(this.client instanceof Client)) _error("L'argument #client fourni n'est pas une instance Discord#Client.", 'INVALID_CLIENT');
+        if (!this.mongoUri) throw new TypeError("MongoDB Uri not provided.");
+        if (!(this.client instanceof Client)) throw new Error("No client provided or client is not instanceof Discord#Client.");
     }
 
     /**
-     * Renvoie la date pour un nombre de jours donné à partir de la date actuelle.
-     * @param {number} [days=1] - Le nombre de jours à ajouter (par défaut: 1).
-     * @returns {Date} La date correspondante.
-     */
+     * Return the date for a given number of days from the current date.
+     * 
+     * @param {number} [days=1] - The number of days to add (default: 1).
+     * @returns {Date} The corresponding date.
+    */
     getFromNextDays(days = 1) {
         return new Date(new Date().getTime() + (864e5 * days));
     }
 
     /**
-     * Récupère les diffusions d'animes pour un jour donné et gère les notifications pour les nouveaux épisodes.
-     * 
+     * Gets anime broadcasts for a given day and handles notifications for new episodes.
+     *
      * @async
-     * @param {string} nextDay - La date du jour pour lequel récupérer les programmes d'anime (au format "YYYY-MM-DD").
-     * @param {number} page - La page de résultats à récupérer (par défaut: 1).
+     * @param {string} nextDay - The date for which to retrieve anime programs (in "YYYY-MM-DD" format).
+     * @param {number} page - The page of results to retrieve (default: 1).
      * @returns {void}
-     */
+    */
     async handleSchedules(nextDay, page) {
         const animes = await fetchAnime(query, { page, nextDay });
 
@@ -98,7 +99,7 @@ class Scheduler extends EventEmitter {
                     clearTimeout(notification);
 
                     if (this.log) {
-                        console.log("\x1b[33[Scheduler] \x1b[0mTous les animes du jour ont été diffusés.");
+                        console.log("\x1b[33[Scheduler] \x1b[0mAll the anime of the day have been broadcast.");
                     }
                 }
             };
@@ -112,17 +113,17 @@ class Scheduler extends EventEmitter {
     }
 
     /**
-     Définit le salon des notifications d'anime pour le serveur spécifié.
-     *
-     * @async
-     * @function setChannel
-     * @param {Guild} guild - L'objet Guild du serveur concerné.
-     * @param {(TextChannel|NewsChannel)} channel - Salon à définir en tant que salon de notification. Discord#TextChannel / Discord#NewsChannel
-     * @returns {Promise<{ error: boolean, message: string, code: number }>} - Une promesse qui retourne un objet contenant des informations sur la réussite ou l'échec de l'opération.
+      Sets the anime notification channel for the specified server.
+      *
+      * @async
+      * @function setChannel
+      * @param {Guild} guild - The Guild object of the server concerned.
+      * @param {(TextChannel|NewsChannel)} channel - Channel to set as notification channel. Discord#TextChannel / Discord#NewsChannel
+      * @returns {Promise<{ error: boolean, message: string, code: number }>} - A promise that returns an object containing information about the success or failure of the operation.
     */
     async setChannel(guild, channel) {
-        if (!guild || !(guild instanceof Guild)) _error('Guild not provided or not instance of Discord#Guild.', errorCode.INVALID_GUILD);
-        if (!channel || !(channel instanceof TextChannel) && !(channel instanceof NewsChannel)) _error('Channel invalide.', 'INVALID_CHANNEL');
+        if (!guild || !(guild instanceof Guild)) throw new TypeError('Guild not provided or not instance of Discord#Guild.');
+        if (!channel || !(channel instanceof TextChannel) && !(channel instanceof NewsChannel)) throw new TypeError('Channel not provided or not instanceof Discord#TextChannel | Discord#NewsChannel.');
 
         const guildDb = await getDatabase(guild.id, true);
         guildDb.channel = channel.id;
@@ -132,18 +133,17 @@ class Scheduler extends EventEmitter {
     }
 
     /**
-     * Définit le rôle des notifications d'anime pour le serveur spécifié.
-     *
-     * @async
-     * @function setRole
-     * @param {Guild} guild - L'objet `Guild` représentant le serveur.
-     * @param {Role} role - Discord#Role | Le rôle qui sera mentionner lorsqu'un anime sera diffusé.
-     * @returns {Promise<{ error: boolean, message: string, code: number }>} Un objet contenant un booléen d'erreur, un message et un code de statut.
-     * @throws {TypeError} Si `guild` n'est pas une instance de `Discord#Guild` ou si `role` n'est pas une instance de `Discord#Role`.
+      * Sets the role of anime notifications for the specified server.
+      *
+      * @async
+      * @function setRole
+      * @param {Guild} guild - The `Guild` object representing the server.
+      * @param {Role} role - Discord#Role | The role that will be mentioned when an anime airs.
+      * @returns {Promise<{ error: boolean, message: string, code: number }>} An object containing an error boolean, message, and status code.
     */
     async setRole(guild, role) {
-        if (!guild || !(guild instanceof Guild)) _error('Guild not provided or not instance of Discord#Guild.', errorCode.INVALID_GUILD)
-        if (!role || !(role instanceof Role)) _error('Rôle invalide.', 'INVALID_ROLE');
+        if (!guild || !(guild instanceof Guild)) throw new TypeError('Guild not provided or not instance of Discord#Guild.')
+        if (!role || !(role instanceof Role)) throw new TypeError('Role not provided or not instanceof Discord#Role.');
 
         const guildDb = await getDatabase(guild.id, true);
         guildDb.role = role.id;
@@ -153,22 +153,16 @@ class Scheduler extends EventEmitter {
     }
 
     /**
-     * Ajoute un anime à la liste du serveur spécifié.
-     * 
+     * Adds an anime to the list of the specified server.
+     *
      * @async
      * @function addAnime
-     * @param {Guild} guild - Le serveur pour lequel on veut ajouter l'anime.
-     * @param {string|number} anime - Le nom ou l'ID de l'anime que l'on veut ajouter depuis https://anilist.co
-     * @returns {Promise<Object>} Une promesse qui se résoud avec un objet contenant les informations suivantes :
-     *      - error {boolean} - Indique si une erreur s'est produite ou non.
-     *      - message {string} - Un message décrivant le résultat de l'opération.
-     *      - code {string} - Un code indiquant le résultat de l'opération.
-     *      - database {GuildDatabase} - La base de données mise à jour, si l'opération a réussi.
-     *      - anime_added {number} - Informations sur l'anime ajouté, si l'opération a réussi.
-     */
+     * @param {Guild} guild - The server for which we want to add the anime.
+     * @param {string|number} anime - The name or ID of the anime we want to add from https://anilist.co
+    */
     async addAnime(guild, anime) {
-        if (!guild || !(guild instanceof Guild)) _error('Guild not provided or not instance of Discord#Guild.', errorCode.INVALID_GUILD);
-        if (!anime) _error('Anime à ajouter non fourni.', 'ANIME_NOT_FOUND');
+        if (!guild || !(guild instanceof Guild)) _error('Guild not provided or not instance of Discord#Guild.');
+        if (!anime) throw new TypeError('Anime not provided.');
 
         let guildDb = await getDatabase(guild.id, true);
 
@@ -180,47 +174,43 @@ class Scheduler extends EventEmitter {
                 search: anime,
                 type: 'ANIME'
             }).then(e => { return Number(e.data.Media?.id) }).catch(e => null);
-            if (!anime) return {
-                error: true,
-                message: 'Anime introuvable.',
-                code: errorCode.ANIME_NOT_FOUND
-            }
+            if (!anime) throw new Error('Anime not found.');
         };
 
         anime = Number(anime);
 
-        if (guildDb.data.includes(anime)) _error('Anime déjà dans la liste du serveur.', 'ALREADY_IN_DATABASE');
+        if (guildDb.data.includes(anime)) throw new Error('Anime is already in list.');
 
         let query = await fetchAnime(AnimeQueryIds, { ids: anime });
-        if (query.errors || !query.data.Page.media.length) _error('Anime introuvable.', 'ANIME_NOT_FOUND');
+        if (query.errors || !query.data.Page.media.length) throw new Error('Anime not found.');
 
         query = query.data.Page.media[0];
 
-        if (query.status === 'FINISHED') _error('La diffusion de cet anime a déjà pris fin.', 'ANIME_FINISHED');
-        if (query.status === 'CANCELLED') _error('La diffusion de cet anime a été annulé.', 'ANIME_CANCELLED');
+        if (query.status === 'FINISHED') return { message: 'The airing of this anime has been finished.', code: 'FINISHED', anime: query };
+        if (query.status === 'CANCELLED') return { message: 'The airing of this anime has been canceled.', code: 'ANIME_CANCELLED', anime: query };
 
         guildDb.data.push(anime);
 
         await guildDb.save().catch(e => console.log('Scheduler#addAnime Error : ', e));
-        return guildDb;
+        return { data: guildDb, anime: query };
     }
 
     /**
-     * Retire un anime de la liste des animes suivis pour un serveur donné.
-     *
+     * Removes an anime from the specified guild's anime list.
+     * 
      * @async
-     * @param {Guild} guild - L'objet serveur Discord.
-     * @param {string} animeId - L'identifiant de l'anime à supprimer.
-     * @returns {Promise<{error: boolean, message: string, code: number}>}
-     */
+     * @function
+     * @param {Guild} guild - The guild to remove the anime from.
+     * @param {string} animeId - The ID or name of the anime to remove.
+     * @returns {Object} - The updated anime list for the guild.
+    */
     async removeAnime(guild, animeId) {
-        if (!guild || !(guild instanceof Guild)) _error('Guild not provided or not instance of Discord#Guild.', errorCode.INVALID_GUILD);
-        if (!animeId) _error('Anime introuvable.', 'ANIME_NOT_FOUND');
+        if (!guild || !(guild instanceof Guild)) throw new TypeError('Guild not provided or not instance of Discord#Guild.', errorCode.INVALID_GUILD);
+        if (!animeId) throw new TypeError('Anime not found.');
 
         let guildDb = await getDatabase(guild.id);
         if (!guildDb) _error('Anime server list is empty.', 'EMPTY_LIST');
 
-        // Récupère l'identifiant de l'anime à partir de son nom ou de son URL
         let anime = Number(animeId);
         if (!anime) {
             if (animeId.match(ANILIST_REGEX)) {
@@ -228,15 +218,11 @@ class Scheduler extends EventEmitter {
             } else if (animeId.match(MAL_REGEX)) {
                 anime = animeId.match(MAL_REGEX)[0];
             } else {
-                const result = await fetchAnime(AnimeQuery, {
+                anime = await fetchAnime(AnimeQuery, {
                     search: animeId,
                     type: 'ANIME'
-                });
-                if (result.data.Media.id) {
-                    anime = Number(result.data.Media.id);
-                } else {
-                    _error('Anime introuvable.', 'ANIME_NOT_FOUND');
-                }
+                }).then(res => { return Number(res.data.Media?.id) }).catch(e => null);
+                if (!anime) throw new Error('Anime not found.');
             }
         }
 
@@ -249,10 +235,12 @@ class Scheduler extends EventEmitter {
     }
 
     /**
-     * Récupère la liste d'anime regardés par le serveur.
+     * Retrieves a list of anime for the specified guild.
      * 
-     * @param {Guild} guild - Le serveur pour lequel récupérer la liste d'anime.
-     * @returns {Promise<Array>} La liste d'anime regardés par le serveur.
+     * @async
+     * @function
+     * @param {Guild} guild - The guild to retrieve anime from.
+     * @returns {Array<Object>} - An array of anime objects.
     */
     async list(guild) {
         if (!guild || !(guild instanceof Guild)) throw new TypeError('Guild not provided or not instance of Discord#Guild.');
@@ -283,10 +271,12 @@ class Scheduler extends EventEmitter {
     }
 
     /**
-     * Supprime les données d'un serveur de la base de données.
-     * 
-     * @param {Guild} guild - Le serveur dont les données doivent être supprimées.
-     * @returns {Object} - Informations conçernant le résultat de la fonction.
+    Deletes a guild's data from the database.
+
+    * @async
+    * @function delete
+    * @param {Guild} guild - The guild to delete data for.
+    * @returns {Promise<Database>} A Promise that resolves with the deleted guild database.
     */
     async delete(guild) {
         if (!guild || !(guild instanceof Guild)) throw new TypeError('Guild not provided or not instance of Discord#Guild.');
@@ -299,23 +289,19 @@ class Scheduler extends EventEmitter {
     }
 
     /**
-     * Modifie le mode de notification du serveur spécifié.
-     *
+     * Sets the notification mode for a guild in the database.
+     * 
      * @async
      * @function setMode
-     *
-     * @param {Guild} guild - Le serveur pour lequel on veut définir le mode de notification.
-     * @param {string} mode - Le mode de notification à définir ('all' pour recevoir toutes les notifications ou 'list' pour les notifications des animés présents dans la liste).
-     *
-     * @returns {Promise<{ error: boolean, message: string, code: string }>} Un objet qui indique si une erreur est survenue, un message associé et un code de retour.
-     * Si tout s'est bien passé, le code de retour est `UPDATED` et le message indique le mode qui a été défini.
-     * Si une erreur est survenue, le code de retour est : `INVALID_MODE` si le mode est invalide ou `DB_ERROR` si une erreur est survenue lors de la modification de la base de données.
-     */
+     * @param {Guild} guild - The guild to set the mode for.
+     * @param {string} mode - The notification mode to set.
+     * @returns {Promise<Database>} A Promise that resolves with the updated guild database.
+    */
     async setMode(guild, mode) {
-        if (!guild || !(guild instanceof Guild)) _error('Guild not provided or not instance of Discord#Guild.', errorCode.INVALID_GUILD);
+        if (!guild || !(guild instanceof Guild)) throw new TypeError('Guild not provided or not instance of Discord#Guild.');
 
         const modes = ['all', 'list'];
-        if (!modes.includes(mode)) _error('Mode de notification non supporté.', 'INVALID_MODE');
+        if (!modes.includes(mode)) new TypeError('Notification mode not supported.');
 
         const guildDb = await getDatabase(guild.id, true);
         guildDb.mode = mode;
@@ -325,7 +311,7 @@ class Scheduler extends EventEmitter {
     }
 
     /**
-     * Initialise le scheduler
+     * Initialize the Scheduler
      * 
      * @async
      * @function init
