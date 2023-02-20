da5f3e281a7eab948fb5156bb31beea2a72bfb5f
zeleffoff <raphaelamoro330@gmail.com>
2023-02-20 20:38:07 +0100

    change langage

--- src/Utils/checkUpdate.js
+++ src/Utils/checkUpdate.js
@@ -2,7 +2,7 @@ async function _checkUpdate() {
     if (!require("node-fetch")) return;
     const packageData = await require("node-fetch")(`https://registry.npmjs.com/discord-anime-scheduler`).then((text) => text.json());
     if (require("../../package.json").version !== packageData["dist-tags"].latest) {
-        console.log("\x1b[31m [Scheduler]", "\x1b[0m Une nouvelle version de \x1b[34mdiscord-anime-scheduler\x1b[0m est disponible ! Pour l'installer, faites ", "\x1b[36mnpm i discord-anime-scheduler@latest");
+        console.log("\x1b[31m [Scheduler]", "\x1b[0m New version of \x1b[34mdiscord-anime-scheduler\x1b[0m has available ! \x1b[36mnpm i discord-anime-scheduler@latest");
     }
 }
 
